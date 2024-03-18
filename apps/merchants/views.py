a9af8295b7a5e814cd1b3d6ca6def3e046497dde
LebohangMhlane <75252307+LebohangMhlane@users.noreply.github.com>
2024-03-18 12:07:38 +0200

    cleanup commit

--- apps/merchants/views.py
+++ apps/merchants/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
 
 from apps.merchants.models import Merchant
 from apps.merchants.serializers.merchant_serializer import MerchantSerializer
