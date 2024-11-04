56f61ac8815579572d5e886e0e709724765c0215
LebohangMhlane <75252307+LebohangMhlane@users.noreply.github.com>
2024-11-04 19:15:49 +0200

    fixed the url

--- apps/price_comparison/urls.py
+++ apps/price_comparison/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProductSearchView
 
 urlpatterns = [
-    path('search/<str:query>', ProductSearchView.as_view(), name='search_products'),
+    path('<str:query>', ProductSearchView.as_view(), name='search_products'),
 ]
