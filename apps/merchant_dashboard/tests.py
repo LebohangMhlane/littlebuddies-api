5ffe871739d66ab6f9aee7e8a478fe369e9e6e8e
LebohangMhlane <75252307+LebohangMhlane@users.noreply.github.com>
2024-11-28 20:41:28 +0200

    successful authentication

--- apps/merchant_dashboard/tests.py
+++ apps/merchant_dashboard/tests.py
@@ -1,6 +1,7 @@
 
 from rest_framework.reverse import reverse
 
+from apps.merchants.models import Branch
 from global_test_config.global_test_config import GlobalTestCaseConfig
 
 
@@ -10,12 +11,18 @@ class MerchantDashboardTestCase(GlobalTestCaseConfig):
 
         # create a merchant user account:
         merchant_user_account = self.create_merchant_user_account()
-        
+
         # create a merchant business:
         merchant_business = self.create_merchant_business(merchant_user_account)
 
         # create a 2nd branch:
         second_branch = self.create_a_branch(merchant_business)
+
+        # set branches permitted to user:
+        merchant_user_account.permitted_branches.add(
+            Branch.objects.get(id=1)
+        )
+        merchant_user_account.save()
 
         # sign in as a merchant:
         token = self.login_as_merchant()
