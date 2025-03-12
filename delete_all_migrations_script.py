7ed5247a98638c8d133cd3080cb08f3533610746
LebohangMhlane <75252307+LebohangMhlane@users.noreply.github.com>
2025-03-12 21:52:55 +0200

    delete all migrations scripts

---
+++ delete_all_migrations_script.py
@@ -0,0 +1,22 @@
+import os
+import glob
+
+def delete_migration_files():
+    for root, dirs, files in os.walk(os.getcwd()):
+        if "migrations" in root.split(
+            os.sep
+        ):  # Ensure we're inside a migrations folder
+            for file in files:
+                if (
+                    file.endswith(".py")
+                    and file != "__init__.py"
+                    or file.endswith(".pyc")
+                ):
+                    file_path = os.path.join(root, file)
+                    os.remove(file_path)
+                    print(f"Deleted: {file_path}")
+
+
+if __name__ == "__main__":
+    delete_migration_files()
+    print("All migration files deleted successfully.")
