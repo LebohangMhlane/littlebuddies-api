293e0ca77e1a84d13177238d7f844cf49cbc0449
LebohangMhlane <75252307+LebohangMhlane@users.noreply.github.com>
2025-04-01 15:02:52 +0200

    App manager formatting and restrictions

--- apps/app_manager/models.py
+++ apps/app_manager/models.py
@@ -7,3 +7,6 @@ from django.db import models
 class AppManager(models.Model):
 
     maintenance_mode_on = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return "App Manager"
