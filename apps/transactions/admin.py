1b72863c1d61b1fbd053ea9dfb429ac3dada7dab
Authurton Lunga <authurton@map2tech.com>
2025-04-06 15:43:59 +0200

    transactions permission fixes

--- apps/transactions/admin.py
+++ apps/transactions/admin.py
@@ -1,4 +1,3 @@
-
 from apps.transactions.models import Transaction
 from django.contrib import admin
 import custom_admin_site
@@ -9,19 +8,17 @@ class TransactionAdmin(admin.ModelAdmin):
     search_fields = ['reference', 'customer__email', 'customer__username']
     
     def get_readonly_fields(self, request, obj=None):
-        if not request.user.useraccount.is_super_user:
-            return [field.name for field in self.model._meta.fields] + ['products_ordered']
-        return self.readonly_fields
+        # Make all fields read-only for everyone
+        return [field.name for field in self.model._meta.fields] + ['products_ordered']
     
+    # Disable change permission for everyone
     def has_change_permission(self, request, obj=None):
-        if request.user.useraccount.is_super_user:
-            return True
         return False
     
     def has_add_permission(self, request):
-        return request.user.useraccount.is_super_user
+        return False
     
     def has_delete_permission(self, request, obj=None):
-        return request.user.useraccount.is_super_user
+        return False
 
-custom_admin_site.custom_admin_site.register(Transaction)
+custom_admin_site.custom_admin_site.register(Transaction, TransactionAdmin)
